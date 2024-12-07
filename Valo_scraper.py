c3a4c413d1b04bb5b8330a7b9661816338422feb
Diace <diace7zworkz@gmail.com>
2024-12-07 11:47:42 +0700

    Update Valo_scraper.py
    >Add sidebar function to scrape side bar on webpage
    >side bar contain elements, such: Accuracy, Roles, Weapons, and Maps
    > Will update to scrap ID list

--- Valo_scraper.py
+++ Valo_scraper.py
@@ -17,6 +17,7 @@ def mainbar(link,driver):
     from selenium.webdriver.common.by import By
     from selenium.webdriver.support.ui import WebDriverWait
     from selenium.webdriver.support import expected_conditions as EC
+    import undetected_chromedriver as uc
     try:
         driver.get(link)
     except:
@@ -37,7 +38,7 @@ def mainbar(link,driver):
         try:
             # Tunggu sampai elemen dengan XPath tertentu muncul
             X_Path = i
-            element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_Path)))
+            element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, X_Path)))
             value = element.text
             overview.append(value)
             
@@ -48,8 +49,8 @@ def mainbar(link,driver):
     for i in range(1,5):
         try:
             X_path = f'//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div[{i}]/div/div[2]/span[2]/span'
-            element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
-            overview.append(value)
+            element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, X_path)))
+            overview.append(element.text)
             overview
         except Exception as e:
             
@@ -60,7 +61,7 @@ def mainbar(link,driver):
         try:
             X_Path = f'//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[2]/div[1]/div[1]/div[5]/div[{i}]/div/div[2]/span[2]/span'
                      #//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[2]/div[1]/div[1]/div[5]/div[1]/div/div[2]/span[2]/span
-            element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
+            element = WebDriverWait(driver, 2).until(EC.presence_of_element_located((By.XPATH, X_path)))
             overview.append(element.text)
             
                        #//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[2]/div[1]/div[1]/div[5]/div[1]/div/div[2]/span[2]/span
@@ -70,7 +71,7 @@ def mainbar(link,driver):
             
             try:
                 X_Path = f'//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[2]/div[1]/div[1]/div[5]/div[{i}]/div/div[1]/span[2]/span'
-                element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, X_path)))
+                element = WebDriverWait(driver,2).until(EC.presence_of_element_located((By.XPATH, X_path)))
                 overview.append(element.text)
                 
             except Exception as e:
@@ -106,10 +107,115 @@ def mainbar(link,driver):
                 overview.append(value)
             except Exception as e:
                 overview.append(float("nan"))
-    print(overview)
-    
-    
-    
-    
-    
     return overview
+
+def sidebar(link,driver):
+    driver.get(link)
+    xpath='//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div'
+    driver.implicitly_wait(3)
+    elements= driver.find_elements(by='xpath', value=xpath)
+    section=[]
+    for i in elements:
+        judul = i.text
+
+        index=(judul.find("""\n"""))
+        section.append((i.text)[0:index])
+
+    accuracy_list = []
+    roles_list = []
+    top_weapons_list = []
+    top_map_list=[]
+    if ("Accuracy" or "Accurac") in section:
+        if "Accurac" in section:
+            number = section.index("Accurac")
+        else:
+            number = section.index("Accuracy")
+
+        xpath_accuracy = f'//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[{number+1}]/'
+        """
+        //*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[2]/div[1]/table/tbody/tr[1]/td[1]
+        //*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[2]/div[1]/table/tbody/tr[1]/td[2]
+        //*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[2]/div[1]/table/tbody/tr[2]/td[1]
+
+        //*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[2]/div[1]/table/tbody/tr[3]/td[1]
+
+        """
+
+        for row in range(1,4):    
+            for col in range(1,3):
+                try:
+                    list_path = f'div[1]/table/tbody/tr[{row}]/td[{col}]'
+                    x_path = xpath_accuracy + list_path
+                    element = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.XPATH, x_path)))
+                    accuracy_list.append(element.text)
+                except:
+                    accuracy_list.append(float("nan"))
+
+    else:
+        accuracy_list = [float("nan")]*6
+
+    if "Roles" in section:
+        number = section.index("Roles")
+        xpath_roles = f'//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[{number+1}]/'
+
+        for i in range(1,5):
+            path_tail = [f'div/div[{i}]/h5',f'div/div[{i}]/div[2]/div[1]/span[1]', f'div/div[{i}]/div[2]/div[1]/span[2]',
+                         f'div/div[{i}]/div[2]/div[2]/span[1]', f'div/div[{i}]/div[2]/div[2]/span[2]']
+            for tail in path_tail:
+                try: 
+                    x_path = xpath_roles+tail
+                    element = driver.find_element(by='xpath', value=x_path).text
+                    roles_list.append(element)
+                except:
+                    roles_list.append(float("nan"))
+    else:
+        roles_list = [float("nan")]*4*5
+
+
+    if "Top Weapons" in section:
+        number = section.index("Top Weapons")
+        xpath_top_weapon = f'//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[{number+1}]/div/'
+        for i in range(1,4):
+            path_tail = [f'div[{i}]/div[1]/div[1]',f'div[{i}]/div[1]/div[2]',
+                         f'div[{i}]/div[2]/div[1]/span[1]', f'div[{i}]/div[2]/div[1]/span[2]', f'div[{i}]/div[2]/div[1]/span[3]',
+                         f'div[{i}]/div[3]/span[2]']
+            for tail in path_tail:
+                try:
+                    x_path = xpath_top_weapon+tail
+                    element = driver.find_element(by='xpath', value=x_path).text
+                    top_weapons_list.append(element)
+
+                except:
+                    top_weapons_list.append(float("nan"))
+    else:
+        top_weapons_list = [float("nan")]*3*6
+
+    if "Top Maps" in section:
+        number = section.index("Top Maps")
+        xpath_top_map = f'//*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[{number+1}]/div'
+
+        """
+        //*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[5]/div/div[{i}]/div[1] Map name
+        //*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[5]/div/div[{i}]/div[2]/div[1] Winrate
+        //*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[5]/div/div[{i}]/div[2]/div[2] win-lose
+
+        //*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[5]/div/div[3]/div[1]
+        //*[@id="app"]/div[2]/div[3]/div/main/div[3]/div[2]/div[2]/div[1]/div[5]/div/div[8]/div[1]
+
+        """
+        for i in range(1,8):
+            path_tail = [f'/div[{i+1}]/div[1]',
+                         f'/div[{i+1}]/div[2]/div[1]',
+                         f'/div[{i+1}]/div[2]/div[2]']
+            for tail in path_tail:
+                try:
+                    x_path = xpath_top_map+tail
+                    element = driver.find_element(by='xpath', value=x_path).text
+                    top_map_list.append(element)
+                except:
+                    top_map_list.append(float("nan"))
+    else:
+        top_map_list = [float("nan")]*7*3
+    sidebar_overview = accuracy_list+roles_list+top_weapons_list+top_map_list
+    
+    return sidebar_overview
